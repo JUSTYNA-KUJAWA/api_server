9bb8bde6cf4d21b9aed05873daaedce6be36c7ef
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-04-28 22:15:03 +0200

    freeSeats

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -32,6 +32,7 @@ class SeatChooser extends React.Component {
 
     const { prepareSeat } = this;
     const { requests, seats, chosenDay } = this.props;
+    const freeSeats = 50 - seats.filter(seat => seat.day === chosenDay).length;
 
     return (
       <div>
@@ -41,7 +42,7 @@ class SeatChooser extends React.Component {
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i + 1))}</div>}
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} />}
         {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert>}
-        {(requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <h5>Seats taken: {seats.filter(seat => seat.day === chosenDay).length}/50</h5> }
+        <h4>{`Free seats: ${freeSeats}/50`} </h4>
       </div>
     )
   };

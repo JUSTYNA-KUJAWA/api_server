e850e32c85aa4e34e7b34855a4ebc3cc16ae784e
Justyna KUJAWA <justy.kujawa@gmail.com>
2022-05-01 17:44:33 +0200

    29.5

--- controllers/testimonials.controller.js
+++ controllers/testimonials.controller.js
@@ -1,5 +1,4 @@
 const Testimonial = require('../models/testimonial.model');
-
 exports.getAll = async (req, res) => {
     try {
       res.json(await Testimonial.find());
@@ -7,17 +6,6 @@ exports.getAll = async (req, res) => {
     catch(err) {
       res.status(500).json({ message: err });
     }
-}
-
-exports.getById = async (req, res) => {
-    try {
-      const tes = await Testimonial.findById(req.params.id);
-      if(!tes) res.status(404).json({ message: 'Not found' });
-      else res.json(tes);
-    }
-    catch(err) {
-      res.status(500).json({ message: err });
-    }  
 }
 
 exports.getRandom = async (req, res) => {
@@ -33,6 +21,17 @@ exports.getRandom = async (req, res) => {
     }
   }
 
+exports.getById = async (req, res) => {
+    try {
+      const tes = await Testimonial.findById(req.params.id);
+      if(!tes) res.status(404).json({ message: 'Not found' });
+      else res.json(tes);
+    }
+    catch(err) {
+      res.status(500).json({ message: err });
+    }  
+}
+
 exports.postTes = async (req, res) => {
     try {
       const { id, author, text } = req.body;
@@ -43,10 +42,9 @@ exports.postTes = async (req, res) => {
       res.status(500).json({ message: err });
     }
   }
-
 exports.putTes = async (req, res) => {
     const { id, author, text } = req.body;
-
+  
     try {
       const tes = await Testimonial.findById(req.params.id);
       if(tes) {
@@ -59,7 +57,6 @@ exports.putTes = async (req, res) => {
       res.status(500).json({ message: err });
     }
 }
-
 exports.deleteTes = async (req, res) => {
     try {
       const tes = await Testimonial.findById(req.params.id);
--- routes/testimonials.routes.js
+++ routes/testimonials.routes.js
@@ -1,19 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const TestimonialController = require('../controllers/testimonials.controller');
-
 // get all testimonials
 router.get('/testimonials', TestimonialController.getAll);
-
-router.get('/testimonials/:id', TestimonialController.getById);
-
 router.get('/testimonials/random', TestimonialController.getRandom);
-
+router.get('/testimonials/:id', TestimonialController.getById);
 router.post('/testimonials', TestimonialController.postTes);
-
 router.put('/testimonials/:id', TestimonialController.putTes);
-
 router.delete('/testimonials/:id', TestimonialController.deleteTes);
-
-
 module.exports = router;
